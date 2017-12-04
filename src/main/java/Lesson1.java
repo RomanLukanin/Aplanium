1eecf10027e713a6e41b15f4c8faadd8a6ab41ef
RomanLukanin <roman.lukanin@aplana.com>
2017-12-04 17:04:41 +0300

    мой первый коммит

---
+++ src/main/java/Lesson1.java
@@ -0,0 +1,5 @@
+public class Lesson1 {
+    public static void main (String[] args) {
+        System.out.println("Spartak (M) - Zenit 3:1");
+    }
+}
--- src/main/java/base.java
+++
@@ -1,6 +0,0 @@
-public class base {
-    public static void main (String[] args) {
-             System.out.println("Spartak (M) - Zenit 3:1");
-        //System.out.println();
-    }
-}
